0098f3f67762c38b9543c4800bd207dc4835ec36
Daniel Berry <daniel@berrymediagroup.com>
2012-03-31 15:08:42 -0500

    updated auth module reset password view

--- fuel/app/modules/auth/views/auth/reset_password.php
+++ fuel/app/modules/auth/views/auth/reset_password.php
@@ -6,40 +6,40 @@
         </div>
 
         <!-- sysmessage if any. !-->
-	    {{message_render()}}
+	    <?php echo Message::render(); ?>
 
 	    <form name="form-reset-password" id="form-reset-password" method="post" class="form-horizontal" action="">
 
-            <div class="control-group {{ errors['email'] ? 'error' : '' }}">
-                <label class="control-label" for="email">{{lang('auth.fields.email')}}</label>
+            <div class="control-group <?php echo !empty($errors['email']) ? 'error' : ''; ?>>
+                <label class="control-label" for="email"><?php echo \Lang::get('auth.fields.email'); ?></label>
 
                 <div class="controls">
-                    <input type="email" class="input-xlarge" id="email" name="email" value="{{input_post('email', '')}}">
+                    <input type="email" class="input-xlarge" id="email" name="email" value="<?php echo \Input::post('email', ''); ?>">
                 </div>
             </div>
 
-            <div class="control-group {{ errors['password'] ? 'error' : '' }}">
-                <label class="control-label" for="password">{{lang('auth.fields.password')}}</label>
+            <div class="control-group <?php echo !empty($errors['password']) ? 'error' : ''; ?>">
+                <label class="control-label" for="password"><?php echo \Lang::get('auth.fields.password'); ?></label>
 
                 <div class="controls">
-                    <input type="password" class="input-xlarge" id="password" name="password" value="{{input_post('password', '')}}">
+                    <input type="password" class="input-xlarge" id="password" name="password" value="<?php echo \Input::post('password', ''); ?>">
                 </div>
             </div>
 
-		    <div class="control-group {{ errors['retype_password'] ? 'error' : '' }}">
-                <label class="control-label" for="retype_password">{{lang('auth.fields.retype_password')}}</label>
+		    <div class="control-group <?php echo !empty($errors['retype_password']) ? 'errors' : ''; ?> ">
+                <label class="control-label" for="retype_password"><?php echo \Lang::get('auth.fields.retype_password'); ?></label>
 
                 <div class="controls">
-                    <input type="password" class="input-xlarge" id="retype_password" name="retype_password" value="{{input_post('retype_password', '')}}">
+                    <input type="password" class="input-xlarge" id="retype_password" name="retype_password" value="<?php echo \Input::post('retype_password', ''); ?>">
                 </div>
             </div>
 
             <div class="form-actions">
-                <button type="submit" class="btn btn-large gray">{{lang('auth.fields.button_reset_password')}}</button>
+                <button type="submit" class="btn btn-large gray"><?php echo \Lang::get('auth.fields.button_reset_password'); ?></button>
 
                 <span class="pull-right form-action-link-align">
-                    <i class="icon-user"></i> Don't Have an Account? <a href="{{router_get('auth/register')}}">Sign Up!</a> <br />
-	                <i class="icon-exclamation-sign"></i> Remembered Your Password? <a href="{{router_get('auth/register')}}">Login!</a>
+                    <i class="icon-user"></i> Don't Have an Account? <a href="<?php echo Router::get('auth/register'); ?>">Sign Up!</a> <br />
+	                <i class="icon-exclamation-sign"></i> Remembered Your Password? <a href="<?php echo \Router::get('auth/login'); ?>>Login!</a>
                 </span>
             </div>
         </form>
